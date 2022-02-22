6de1c9671946fd5ab3e0fbd5bca9f8f7a64928a3
dreamer59988 <TCK599@HOTMAIL.COM>
2022-02-22 18:00:51 +0800

    Create lesson2.py

---
+++ lesson2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 22 17:34:29 2022
+,,,
+@author: user
+"""
+
+n1 = input('Enter the first number:')
+n2 = input('Enter the second number:')
+
+sum=int(n1)+int(n2)
+
+print (f"n1+n2={sum*3}")
