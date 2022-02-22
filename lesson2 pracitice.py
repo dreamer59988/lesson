54b90d193c6df39455b4b2b71c9a84a2041ee739
dreamer59988 <TCK599@HOTMAIL.COM>
2022-02-22 18:10:14 +0800

    Create lesson2 pracitice.py

---
+++ lesson2 pracitice.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 22 18:05:37 2022
+
+@author: user
+"""
+
+y=0
+for i in range(1,6):
+    x= input(f'enter {i} number:')
+    y=y+int(x)
+print (f'the result={y}')
