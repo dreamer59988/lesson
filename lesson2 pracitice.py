c181c03e5ee56b4e73905cee873154b38f9d7af3
dreamer59988 <TCK599@HOTMAIL.COM>
2022-02-24 13:12:49 +0800

    Update lesson2 pracitice.py

--- lesson2 pracitice.py
+++ lesson2 pracitice.py
@@ -10,15 +10,18 @@ Created on Tue Feb 22 18:05:37 2022
   #  x= input(f'enter {i} number:')
   #  y=y+int(x)
 #print (f'the result={y}')
-
+'''
 a="*"
 b=" "
 c=1
 d=6
 e=4
 i=1
+p=" "
 tmax=0
 tmin=0
+
+
 print(f"1 {b} 1")
 #print("\n")
 print("\n")
@@ -37,4 +40,43 @@ for y in range(c,e):
               print(a,end="")
         else:
               print(b,end="")
+        if i== d:
+            print("")
+          '''  
+
+
+
+
+def triangle():
+    a='*'
+    b=' '
+    c=1
+    d=50
+    e=25
+    i=1
+    y=1
+    p=''
+    tmax=0
+    tmin=0
+    for y in range(c,e):
+        if y==1:
+                tmax=d/2
+                tmin=d/2
+        if y!=1:
+            tmax=tmax+1
+            tmin=tmin-1
+        for i in range(c,d):
+            if i>=tmin and i<=tmax:
+                p=a
+            else:
+                p=b
+           
+            print(p,end="")
+            if i==5:
+                print('')
+                
+
     
+d=input('input high:')
+triangle()
+
